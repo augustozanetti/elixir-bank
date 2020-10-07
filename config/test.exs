48c7f5ff93e08f3f7a6215e96ef302ecd7b5930a
Augusto Zanetti <augusto.zanetti@profusion.mobi>
2020-10-07 17:07:51 -0300

    resolve db connection

--- config/test.exs
+++ config/test.exs
@@ -7,7 +7,7 @@ use Mix.Config
 # Run `mix help test` for more information.
 config :bank, Bank.Repo,
   username: "postgres",
-  password: "postgress",
+  password: "postgres",
   database: "bank_test#{System.get_env("MIX_TEST_PARTITION")}",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
