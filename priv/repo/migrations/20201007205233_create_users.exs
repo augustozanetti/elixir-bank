42a5c28220372934092db0078ca5fe95a3ce5447
Augusto Zanetti <augusto.zanetti@profusion.mobi>
2020-10-07 17:59:02 -0300

    feat: create user schema

---
+++ priv/repo/migrations/20201007205233_create_users.exs
@@ -0,0 +1,14 @@
+defmodule Bank.Repo.Migrations.CreateUsers do
+  use Ecto.Migration
+
+  def change do
+    create table(:users) do
+      add :name, :string
+      add :email, :string
+      add :password_hash, :string
+      add :isAdmin, :boolean, default: false, null: false
+
+      timestamps()
+    end
+  end
+end
